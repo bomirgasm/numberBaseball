49251e7757febde2b8b999d6d916c01c26439ec2
bomirgasm <bomirgasm@gmail.com>
2025-03-20 20:52:24 +0900

    스트라이크 & 볼 판정 기능 추가

--- numberBaseball/main.swift
+++ numberBaseball/main.swift
@@ -14,3 +14,15 @@ print("랜덤 숫자: \(randomNumbers)")
 
 let userInput = UserInput().getUserInput()
 print("사용자 입력값: \(userInput)")
+
+let userGuess = userInput.map { Int(String($0))! }
+
+let result = StrikeBallChecker().checkStrikeAndBall(answer: randomNumbers, userGuess: userGuess)
+
+if result.strike == 3 {
+    print("🎉 정답입니다!")
+} else if result.strike == 0 && result.ball == 0 {
+    print("Nothing")
+} else {
+    print("\(result.strike) 스트라이크, \(result.ball) 볼")
+}
